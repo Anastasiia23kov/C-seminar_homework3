3ff8dcbbe0ea4b01b81208c0fc33dea1b71b4898
Anastasiia <penkova23anastasia@gmail.com>
2022-11-29 15:44:22 +0200

    Задача 23. Программа на вход принимает число (N), и выдает таблицу кубов чисел от 1 до N.

---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 23. Программа на вход принимает число (N),
+//и выдает таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.Write($"{Math.Pow(i,3)},");
